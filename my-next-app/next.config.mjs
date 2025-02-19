8d85998791adb640c1296263a2ec3daa247524f8
Mithrildagger <nadimchatellier@gmail.com>
2025-02-19 01:17:49 +0000

    slight fix

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Optional: If you want to use React Strict Mode
-    swcMinify: true,       // Optional: Enables SWC minification for better build performance
-  
-    // Uncomment and configure this if you're using a custom server
-    // target: 'serverless', // Uncomment this line for serverless deployment
-  
-    // Netlify specific configuration (optional)
-    experimental: {
-      outputStandalone: true, // Make the build independent of the server for better performance on Netlify
-    },
-  
-    // You can specify a public path if necessary for your app (optional)
-    // basePath: '/my-base-path',
+    output: 'standalone', // Move this from experimental to here
   };
   
   export default nextConfig;
+  
   
